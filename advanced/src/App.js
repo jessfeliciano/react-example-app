98e4d60e799c7f5f4f8e392f2ba06a81e20f59fc
Jessica Feliciano <jessfeliciano87@gmail.com>
2022-11-05 20:47:49 -0500

    chore: slight clean up to App and how layout is implemented

--- advanced/src/App.js
+++ advanced/src/App.js
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Set from "./pages/Set";
 import Card from "./pages/Card";
+import Layout from './layouts';
 
-// TODO: set up routes here
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/set" element={<Set />} />
-      <Route path="/card" element={<Card />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/set" element={<Set />} />
+        <Route path="/card" element={<Card />} />
+      </Routes>
+    </Layout>
+
   );
 }
 
--- advanced/src/index.js
+++ advanced/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './assets/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Layout from './layouts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Layout>
       <App />
-    </Layout>
   </React.StrictMode>
 );
 
