533858a7f1ab80a63ff7cc839f2651bc5437bde1
Jessica Feliciano <jessfeliciano87@gmail.com>
2022-11-05 20:44:59 -0500

    feat: sets up initial routing

--- advanced/src/App.js
+++ advanced/src/App.js
@@ -1,24 +1,16 @@
-import logo from './assets/logo.svg';
-import './assets/global.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Set from "./pages/Set";
+import Card from "./pages/Card";
 
+// TODO: set up routes here
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/set" element={<Set />} />
+      <Route path="/card" element={<Card />} />
+    </Routes>
   );
 }
 
