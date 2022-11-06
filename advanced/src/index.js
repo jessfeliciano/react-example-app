1358aa718c1e230f6643b38ad4ae1d3708079b4d
Jessica Feliciano <jessfeliciano87@gmail.com>
2022-11-05 20:44:51 -0500

    chore: organizes files and creates basic components for routing purposes

--- advanced/src/index.js
+++ advanced/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './assets/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Layout from './layouts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>
 );
 
---
+++ advanced/src/pages/Set.js
@@ -0,0 +1,5 @@
+const Set = () => {
+
+}
+
+export default Set;
