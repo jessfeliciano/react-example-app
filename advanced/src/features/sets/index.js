15eaf1f4cbe1452badf244e8d1d1040b5f550689
Jessica Feliciano <jessfeliciano87@gmail.com>
2022-11-07 20:27:03 -0600

    feats: initial setup of redux react

---
+++ advanced/src/features/sets/index.js
@@ -0,0 +1,4 @@
+// server component that captures set data
+
+// Generation component made up of set cards of the same generation
+
--- advanced/src/index.js
+++ advanced/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './assets/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './store/reducers/index'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore(rootReducer)
+
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store = {store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
 
